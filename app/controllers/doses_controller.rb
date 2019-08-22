d265943ff5b8563ad668cdd8ea56df11d6b67e4f
Guillaume Zeldine <g.zeldine@hotmail.fr>
2019-08-22 15:07:47 -0300

    Done

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,11 +1,36 @@
 class DosesController < ApplicationController
+  before_action :set_cocktail, only: %i[new create]
+  before_action :find_dose, only: [:destroy]
+
   def new
     @dose = Dose.new
   end
 
   def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
   end
 
   def destroy
+    @dose.destroy
+  end
+
+  private
+
+  def find_dose
+    @dose = Dose.find(params[:id])
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :cocktails do
-    resources :doses, only: %i[new create destroy]
+    resources :doses, only: %i[new create]
   end
+  resources :doses, only: [:destroy]
 end
